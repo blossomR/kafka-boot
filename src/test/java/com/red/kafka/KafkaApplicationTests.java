85b9682b36f9d3267dba51491c5e2ef36917cc58
blossomR <hongqiang102@qq.com>
2020-02-07 22:27:43 +0800

    Initial commit

---
+++ src/test/java/com/red/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
