aefb5476dd8fd7749a68b930d0b7b98763e894a0
Red <redwal@163.com>
2020-02-09 23:47:48 +0800

    水一天，明天好好写

--- src/main/java/com/red/kafka/Controller.java
+++ src/main/java/com/red/kafka/Controller.java
@@ -1,6 +1,7 @@
 package com.red.kafka;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class Controller {
 
-    private final KafkaTemplate<Object, Object> kafkaTemplate;
-
-    public Controller(KafkaTemplate<Object, Object> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    @Autowired
+    private KafkaTemplate  kafkaTemplate;
 
     @GetMapping("/send/{messge}")
     public String send(@PathVariable String messge) {
